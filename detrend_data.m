42f8d68e6ad4643eccbf9f8a4107105d0b1eb039
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2019-10-16 14:16:58 -0400

    Add files via upload

--- detrend_data.m
+++ detrend_data.m
@@ -1,10 +1,16 @@
 % detrend data (1850 - 2100)
 
+vars={'thetao' 'so' 'dissic' 'talk' 'o2' 'po4'};
+
+for l = 1:4 
+    
+    
+
 % define model and variable
 model='GFDL-CM4';
 run='ssp245';
 variant='r1i1p1f1';
-var='thetao';
+var=vars{l};%'thetao';
 if strcmp(run,'historical')
     N0=1;
 else
@@ -14,6 +20,8 @@ end
 % define file name
 fn0=[model,'/',model,'_piControl_',variant,'_2x2L33_ann.nc'];
 fn1=[model,'/',model,'_',run,'_',variant,'_2x2L33_ann.nc'];
+wn=[model,'/',var,'_',model,'_',run,'_',variant,'_2x2L33_ann_dft.mat'];
+disp(['working on ',var]);
 
 % load picontrol data
 pi=ncread(fn0,var,[1 1 1 1],[180 90 33 250]);
@@ -31,7 +39,7 @@ N=size(pi);
 X=[1:N(4)]-0.5;
 tmp=zeros(N);
 for i=1:N(1)
-    disp(['   working on i = ',num2str(i),' ...']);
+%    disp(['   working on i = ',num2str(i),' ...']);
     for j=1:N(2)
         for k=1:N(3)
             if ~isnan(pi(i,j,k,1))
@@ -48,9 +56,11 @@ clear X Y C;
 % drift corrected data
 dataDC=data-tmp(:,:,:,N0:(N0+Nt-1));
 eval([var,'=dataDC;']);
+
+save(wn,'-v7.3',var);
+
+end
+
 disp('done!');
-
-
-
 
 
